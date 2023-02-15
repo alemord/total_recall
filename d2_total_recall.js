ec4159ab3d770a919ad03fbab44b37ed84f7d1ce
alemord <alessandro.mordini@gmail.com>
2023-02-15 15:34:35 -0500

    end of day

--- d2_total_recall.js
+++ d2_total_recall.js
@@ -60,36 +60,83 @@ for (let turtle of ninjaTurtles) {
 
 
 
-////////////////////////////////
-// Methods, Revisited
-////////////////////////////////
+// ////////////////////////////////
+// // Methods, Revisited
+// ////////////////////////////////
 const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
 
-console.log (favMovies [8]);
 
-favMovies.push('La Dolce Vita');
-console.log(favMovies);
+console.log (favMovies.indexOf("Titanic"));
 
 favMovies.sort();
-console.log(favMovies);
-
 
 favMovies.pop();
-console.log (favMovies);
+
+favMovies.push("Guardians of the Galaxy");
 
 favMovies.reverse();
+
+favMovies.shift();
+
+console.log (favMovies.unshift("I am Sam"));
+
+let index = favMovies.indexOf("Django Unchained");
+favMovies.splice(index,1,"Avatar");
+
+let lastHalf = [];
+let length = favMovies.length;
+lastHalf = favMovies.slice (Math.floor(length / 2),length +1); 
+
+console.log (lastHalf);
+
 console.log (favMovies);
+
 
 ////////////////////////////////
 // Where is Waldo
 ////////////////////////////////
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
 
-
+whereIsWaldo.splice (1 ,1 );
+console.log (whereIsWaldo);
 ////////////////////////////////
+
+whereIsWaldo.slice (4);
+console.log (whereIsWaldo);
+
+whereIsWaldo[1][2] = "No One";
+
+console.log(whereIsWaldo[2][1][1]);
+
+
 //  Excited Kitten
 ////////////////////////////////
+for (let i = 0; i < 20; i++) {
+  console.log("Love me, pet me! HSSSSSS!");
+}
+
+///
+
+for (let i = 0; i <= 20; i++) {
+  if (i % 2 === 0) {
+    const messages = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    console.log(messages[randomIndex]);
+  }
+}
 
 
 ////////////////////////////////
 //  Find the Median
 ////////////////////////////////
+const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+
+nums.sort();
+console.log (nums);
+let len = Math.ceil (nums.length/2);
+console.log (len);
+console.log (nums [len]);
+
+//
